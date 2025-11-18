fcb9ecfbc92ad1357552fb62364b1d1d8be05c6c
TiagoDemay <tiago@demay.cloud>
2025-11-18 08:34:06 -0300

    Update endDate time in atualizar_allow_access.py

--- pl_gradebook/atualizar_allow_access.py
+++ pl_gradebook/atualizar_allow_access.py
@@ -36,7 +36,7 @@ def atualizar_allow_access(exam_path, aprovados_path, output_path):
             bloco = {
                 "uids": [email],
                 "startDate": "2025-01-16T08:00:00",
-                "endDate": "2025-11-18T11:00:00",
+                "endDate": "2025-11-18T15:00:00",
                 "password": gerar_senha_aleatoria(),
                 "timeLimitMin": 10,
                 "credit": 100
